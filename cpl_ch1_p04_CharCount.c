ce73bf5a8754f61e8ea13fc7f222e481503a1a08
Fazil <fazilt@gmail.com>
2016-02-28 12:24:55 -0800

    Edit - Character Counting
    using for loop

--- cpl_ch1_p04_CharCount.c
+++ cpl_ch1_p04_CharCount.c
@@ -3,11 +3,13 @@
 /* Count characters in input */
 
 int main(int argc, char* argv[]){
-	long nc;
-	nc =0;
-	
-	while (getchar() != EOF)
-		++nc;
-	printf("Character count %ld\n", nc);
+	//long nc;
+	//nc =0;
+	//while (getchar() != EOF)
+	//	++nc;
+	//printf("Character count %ld\n", nc);
+	double nc;
+	for (nc = 0; getchar() != EOF; ++nc);
+	printf("Charcter count %.0f\n",nc);
 	return 0;
 }
