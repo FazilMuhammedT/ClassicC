f2b8cce7572c20619000ed5bad57ddbb218152fe
Fazil <fazilt@gmail.com>
2016-02-28 10:20:47 -0800

    Edit - Temperature Conversion
    Adding Symbolic Constants

--- cpl_ch1_ex05_TemperatureConversion.c
+++ cpl_ch1_ex05_TemperatureConversion.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 /* Program to print Farhenheit to Celsius */
 
+#define LOWER	0	/* lower limit */
+#define UPPER	300	/* upper limit */
+#define STEP	20	/* step */
+
 int main(){
 	printf("Print Fahrenheit to Celsius using for loop\n");
-	for(int fahr=300; fahr>=0; fahr=fahr-20){
+	for(int fahr = UPPER; fahr >= LOWER; fahr = fahr-STEP){
 		printf("%3d  %6.2f\n",fahr,(5.0/9.0)*(fahr-32));
 	} 
 }
