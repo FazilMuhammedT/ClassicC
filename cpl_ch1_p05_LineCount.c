0d4b912991d7e967f254e6011dde32608292c54b
Fazil <fazilt@gmail.com>
2016-02-28 13:05:10 -0800

    Edit - Count Lines
    Added logic to count blank space and tabs

--- cpl_ch1_p05_LineCount.c
+++ cpl_ch1_p05_LineCount.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 
-/* count lines in input */
+/* count blanks, tabs and newlines in input */
 
 int main(int argc, char *argv[])
 {
-	int c, nl;
+	int c, nb, nt, nl;
+	nb = 0;
+	nt = 0;
 	nl = 0;
 
-	while ((c = getchar()) != EOF)
+	while ((c = getchar()) != EOF){
 		if (c == '\n')
 			nl++;
-	printf("Line count %d\n",nl);
+		else if (c == '\t')
+			nt++;
+		else if (c == ' ')
+			nb++;
+	}
+	printf("Blank space is %d, Tab Count is %d amd Line count is %d\n", nb, nt, nl);
 	
 	return 0;
 }
