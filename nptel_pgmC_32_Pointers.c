3b70460444c1a56d8103a64bf9422a5ce589cd78
Fazil <fazilt@gmail.com>
2016-11-04 16:33:54 -0700

    Edit - Playing with pointers
    Printing arrays using pointer arithmetic method

--- nptel_pgmC_32_Pointers.c
+++ nptel_pgmC_32_Pointers.c
@@ -5,7 +5,7 @@
 **/
 
 int main(){
-	int num[10];
+	int num[] = {1,10,-1,4,6};
 	int *ptr; //Pointer to an integer - contains address of an integer variable.
 	num[1] = 1;
 	ptr = &num[1]; //Assign the address of the num[1]
@@ -23,9 +23,11 @@ int main(){
 	printf("Value of the new integer in num[1] -> %d\n",num[1]);
 
 	num[2] = *ptr; //Assigning the integer value in the pointer to num[2] using dereferencing pointer
-	printf("Value of the  integer in num[2] -> %d\n",num[2]);
+	*ptr = *ptr + 5; //This will add 5 to the value in the address pointed by the ptr
+	printf("Value of the integer in num[1] after addition -> %d\n",num[1]);
+	printf("Value of the integer in num[2] -> %d\n",num[2]);
 
-
+	printf("Printing the values in array using pointer arithmetic -> %d %d %d %d %d\n", *(num), *(num+1), *(num+2), *(num+3), *(num+4));
 
 	return 0;
 }
