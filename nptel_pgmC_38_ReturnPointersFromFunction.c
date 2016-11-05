6140f3c0053b405fac8c517cc12bc979efdf1b77
Fazil <fazilt@gmail.com>
2016-11-05 11:24:19 -0700

    Edit - Return pointer from a function
    Creating a memory in the heap to overcome dangling pointer

--- nptel_pgmC_38_ReturnPointersFromFunction.c
+++ nptel_pgmC_38_ReturnPointersFromFunction.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * This is an example for Dangling Pointer
@@ -11,8 +12,27 @@ int *increment (int n) {
 	return ptr;
 }
 
+int *incrementUsingHeap (int n) {
+	int *ptr;
+	// Allocate one integer on the heap
+	// Treat the return address as an integer pointer
+	ptr = (int *) malloc(sizeof( int ));
+	// Increment the value in heap using dereferncing property
+	*ptr = n + 1;
+	// return will remove the ptr in stack, but the memory allocated in heap stays
+	return ptr;
+}
+
 int main(){
 	int *p = increment(1); 
-	printf("Integer value after incremneting 1 - %d\n",*p);
+	printf("Integer value after incrementing 1 - %d\n",*p);
+
+	p = incrementUsingHeap(1);
+	printf("Integer value after incrementing 1 using heap property - %d\n",*p);
+	// remove the space allocated on heap in the function incrementUsingHeap()
+	// Forget to free memory after use called a memory leak
+	free(p);
+	p = 0;
+
 	return 0;
 }
