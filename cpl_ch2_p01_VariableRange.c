9660590c77a1d291b7f757e327d86de02a403964
Fazil <fazilt@gmail.com>
2016-11-05 08:21:42 -0700

    Edit - Variable Range
    Calcuating variable size using sizeof() operator

--- cpl_ch2_p01_VariableRange.c
+++ cpl_ch2_p01_VariableRange.c
@@ -11,23 +11,28 @@ int main(){
 	
 	printf("Determining the variable range using standard library limits.h\n");
 
-	printf("Size of char %d\n",CHAR_BIT);
-	printf("Size of char max %d\n",CHAR_MAX);
-	printf("Size of char min %d\n",CHAR_MIN);
-	
-	printf("Size of short  max %d\n",SHRT_MAX);
-	printf("Size of short  min %d\n",SHRT_MIN);
-	
-	printf("Size of int max %d\n",INT_MAX);
-	printf("Size of int min %d\n",INT_MIN);
+	printf("Size of char %d bit\n",CHAR_BIT);
+	printf("Size of char using sizeof operator %lu byte\n",sizeof(char));
 
-	printf("Size of long max %ld\n",LONG_MAX);
-	printf("Size of long min %ld\n",LONG_MIN);
+	printf("Value of char max %d\n",CHAR_MAX);
+	printf("Value of char min %d\n",CHAR_MIN);
 	
-	printf("Size of unsigned char %u\n",UCHAR_MAX);
-	printf("Size of unsigned short %u\n",USHRT_MAX);
-	printf("Size of unsigned int %u\n",UINT_MAX);
-	printf("Size of unsigned long %lu\n",ULONG_MAX);
+	printf("Size of short using sizeof operator %lu byte\n",sizeof(short));
+	printf("Value of short  max %d\n",SHRT_MAX);
+	printf("Value of short  min %d\n",SHRT_MIN);
+	
+	printf("Size of int using sizeof operator %lu byte\n",sizeof(int));
+	printf("Value of int max %d\n",INT_MAX);
+	printf("Value of int min %d\n",INT_MIN);
+
+	printf("Size of long using sizeof operator %lu byte\n",sizeof(long));
+	printf("Value of long max %ld\n",LONG_MAX);
+	printf("Value of long min %ld\n",LONG_MIN);
+	
+	printf("Value of unsigned char %u\n",UCHAR_MAX);
+	printf("Value of unsigned short %u\n",USHRT_MAX);
+	printf("Value of unsigned int %u\n",UINT_MAX);
+	printf("Value of unsigned long %lu\n",ULONG_MAX);
 
 	printf("Determining the variable range using direct computation\n");
 	
@@ -49,6 +54,9 @@ int main(){
 	printf("Maximum Unsigned Short %d\n",(unsigned short)~0);
 	printf("Maximum Unsigned Int %u\n",(unsigned int)~0);
 	printf("Maximum Unsigned Long %lu\n",(unsigned long)~0);
+
+	printf("Size of float using sizeof operator %lu byte\n",sizeof(float));
+	printf("Size of double using sizeof operator %lu byte\n",sizeof(double));
 
 	return 0;
 }
