0fc672d3fec1576831a03327980f391b5a916496
Fazil <fazilt@gmail.com>
2016-11-06 10:49:06 -0800

    Edit - Multidimensional Array
    To add sum of values in a row

--- nptel_pgmC_43_MultidimensionalArray.c
+++ nptel_pgmC_43_MultidimensionalArray.c
@@ -14,8 +14,20 @@ void printArray(int array[3][3]){
 	}
 }
 
+//To print the sum of each rows upto n, where n < number of rows.
+void rowSum (int array[][3],int n){
+	int rowSum = 0;
+	for(int i = 0; i < n; i++){
+		rowSum = 0;
+		for(int j = 0; j < 3; j++)
+			rowSum += array[i][j];
+		printf("Sum of %d row is %d\n", i, rowSum);
+	}
+}
+
 int main (){
 	int array[][3] = { {1}, {2, 3}, {3, 4, 5}};
 	printArray(array);
+	rowSum (array, 2);
 	return 0;
 }
